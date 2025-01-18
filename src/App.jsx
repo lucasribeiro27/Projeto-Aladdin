75463d9b937c1323af8bf544e9e1bbe88e46047f
Lucas Ribeiro <lucas027322@gmail.com>
2025-01-18 13:06:06 -0300

    estilização e função de aparecer o gênio

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-
-
-
+import { useState } from 'react'
+import Lampada from './assets/lampada.png'
+import Genio from './assets/genio.png'
+import './Style.scss'
 
 export default function App() {
-  return <main>
+  const [aladdin, setAladdin] = useState(Lampada);
 
-  </main>;
+  // const genioDaLampada = () => {
+  //   setAladdin(Genio);
+  // };
+  const genioDaLampada = () => {
+    setAladdin((prevState) => prevState === Lampada ? Genio : Lampada);  // Alterna entre as imagens
+  };
+
+  return (
+    <main>
+      <h1>&quot;Liberte a magia da programação com a lâmpada de Aladim.&quot;</h1>
+      <img src={aladdin} alt="Imagem lâmpada mágica" />
+      <button onClick={genioDaLampada}>Clique Aqui</button>
+    </main>
+  );
 }
